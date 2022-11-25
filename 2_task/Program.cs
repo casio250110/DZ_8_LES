31847574f66983d4e380ccec6ee7183a54697a7c
casio250110 <casio250110@yandex.ru>
2022-11-26 00:05:48 +0300

    Решили 2 задачу

---
+++ 2_task/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//5 2 6 7
+//Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+void FillArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+
+void SumIndex(int[,] matrix)
+{
+    int sumCurrent = 0;
+    int minSum = 0;
+    int minSumCurrent = 0;
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        minSum += matrix[0, i];
+    }
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sumCurrent += matrix[i, j];
+        }
+        if (sumCurrent < minSum)
+        {
+            minSum = sumCurrent;
+            minSumCurrent = i;
+        }
+        sumCurrent = 0;
+    }
+    Console.Write($"{minSumCurrent + 1} строка ");
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] matr = new int[4, 4];
+FillArray(matr);
+PrintArray(matr);
+Console.WriteLine();
+SumIndex(matr);
